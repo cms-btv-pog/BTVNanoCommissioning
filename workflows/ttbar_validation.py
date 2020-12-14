a801fa5bde53c2639f364a7df08e422a6674e2de
Denise <denise.muller@cern.ch>
2020-12-14 12:37:37 +0100

    first deepcsv var plots; fixed njets and nlep plots

--- workflows/ttbar_validation.py
+++ workflows/ttbar_validation.py
@@ -36,12 +36,21 @@ class NanoProcessor(processor.ProcessorABC):
         jet_phi_axis  = hist.Bin("phi",  r"$\phi$", 60, -3, 3)
         jet_mass_axis = hist.Bin("mass", r"Jet $m$ [GeV]", 100, 0, 50)
         ljpt_axis     = hist.Bin("ljpt", r"Leading jet $p_{T}$ [GeV]", 100, 20, 400)
-                
+        sljpt_axis     = hist.Bin("sljpt", r"Subleading jet $p_{T}$ [GeV]", 100, 20, 400)
+        
         # Define similar axes dynamically
         disc_list = ["btagCMVA", "btagCSVV2", 'btagDeepB', 'btagDeepC', 'btagDeepFlavB', 'btagDeepFlavC',]
         btag_axes = []
         for d in disc_list:
             btag_axes.append(hist.Bin(d, d, 50, 0, 1))        
+        
+        deepcsv_list = ["DeepCSV_trackDecayLenVal_0", "DeepCSV_trackDecayLenVal_1", "DeepCSV_trackDecayLenVal_2", "DeepCSV_trackDecayLenVal_3", "DeepCSV_trackDecayLenVal_4", "DeepCSV_trackDecayLenVal_5", "DeepCSV_trackDeltaR_0", "DeepCSV_trackDeltaR_1", "DeepCSV_trackDeltaR_2", "DeepCSV_trackDeltaR_3", "DeepCSV_trackDeltaR_4", "DeepCSV_trackDeltaR_5"]
+        deepcsv_axes = []
+        for d in deepcsv_list:
+            if "trackDecayLenVal" in d:
+                deepcsv_axes.append(hist.Bin(d, d, 50, 0, 2))
+            else:
+                deepcsv_axes.append(hist.Bin(d, d, 50, 0, 0.3))
 
         # Define histograms from axes
         _hist_jet_dict = {
@@ -50,10 +59,18 @@ class NanoProcessor(processor.ProcessorABC):
                 'phi' : hist.Hist("Counts", dataset_axis, jet_phi_axis),
                 'mass': hist.Hist("Counts", dataset_axis, jet_mass_axis),
             }
-        
+        _hist_deepcsv_dict = {
+                'pt'  : hist.Hist("Counts", dataset_axis, jet_pt_axis),
+                'eta' : hist.Hist("Counts", dataset_axis, jet_eta_axis),
+                'phi' : hist.Hist("Counts", dataset_axis, jet_phi_axis),
+                'mass': hist.Hist("Counts", dataset_axis, jet_mass_axis),
+            }
+ 
         # Generate some histograms dynamically
         for disc, axis in zip(disc_list, btag_axes):
             _hist_jet_dict[disc] = hist.Hist("Counts", dataset_axis, axis)
+        for deepcsv, axis in zip(deepcsv_list, deepcsv_axes):
+            _hist_deepcsv_dict[deepcsv] = hist.Hist("Counts", dataset_axis, axis)
         
         _hist_event_dict = {
                 'njet'  : hist.Hist("Counts", dataset_axis, njet_axis),
@@ -63,12 +80,14 @@ class NanoProcessor(processor.ProcessorABC):
                 'lelpt' : hist.Hist("Counts", dataset_axis, lelpt_axis),
                 'lmupt' : hist.Hist("Counts", dataset_axis, lmupt_axis),
                 'ljpt'  : hist.Hist("Counts", dataset_axis, ljpt_axis),
+                'sljpt'  : hist.Hist("Counts", dataset_axis, sljpt_axis),
             }
         
         self.jet_hists = list(_hist_jet_dict.keys())
+        self.deepcsv_hists = list(_hist_deepcsv_dict.keys())
         self.event_hists = list(_hist_event_dict.keys())
     
-        _hist_dict = {**_hist_jet_dict, **_hist_event_dict}
+        _hist_dict = {**_hist_jet_dict, **_hist_deepcsv_dict, **_hist_event_dict}
         self._accumulator = processor.dict_accumulator(_hist_dict)
 
 
@@ -150,7 +169,7 @@ class NanoProcessor(processor.ProcessorABC):
         # output['pt'].fill(dataset=dataset, pt=selev.Jet.pt.flatten())
         # Fill histograms dynamically  
         for histname, h in output.items():
-            if histname not in self.jet_hists: continue
+            if (histname not in self.jet_hists) and (histname not in self.deepcsv_hists): continue
             # Get valid fields perhistogram to fill
             fields = {k: ak.flatten(sjets[k], axis=None) for k in h.fields if k in dir(sjets)}
             h.fill(dataset=dataset, **fields)
@@ -162,14 +181,15 @@ class NanoProcessor(processor.ProcessorABC):
         def num(ar):
             return ak.num(ak.fill_none(ar[~ak.is_none(ar)], 0), axis=0)
 
-        output['njet'].fill(dataset=dataset,  njet=num(sjets))
-        output['nbjet'].fill(dataset=dataset, nbjet=num(sbjets))
-        output['nel'].fill(dataset=dataset,   nel=num(sel))
-        output['nmu'].fill(dataset=dataset,   nmu=num(smu))
+        output['njet'].fill(dataset=dataset,  njet=flatten(ak.num(sjets)))
+        output['nbjet'].fill(dataset=dataset, nbjet=flatten(ak.num(sbjets)))
+        output['nel'].fill(dataset=dataset,   nel=flatten(ak.num(sel)))
+        output['nmu'].fill(dataset=dataset,   nmu=flatten(ak.num(smu)))
 
         output['lelpt'].fill(dataset=dataset, lelpt=flatten(selev.Electron[:, 0].pt))
         output['lmupt'].fill(dataset=dataset, lmupt=flatten(selev.Muon[:, 0].pt))
         output['ljpt'].fill(dataset=dataset,  ljpt=flatten(selev.Jet[:, 0].pt))
+        output['sljpt'].fill(dataset=dataset,  sljpt=flatten(selev.Jet[:, 1].pt))
 
         return output
 
