ca21ab0bf27c429eea49564bacb07bc43e06571a
Denise <denise.muller@cern.ch>
2021-03-18 19:23:43 +0100

    samples for UL16 Summer19 vs 20 comparison

--- filefetcher/fetch.py
+++ filefetcher/fetch.py
@@ -4,39 +4,23 @@ import argparse
 import pprint
 
 fset = []
-with open("list.txt") as fp: 
+with open("UL16check.txt") as fp: 
     lines = fp.readlines() 
     for line in lines: 
         fset.append(line)
 
 fdict = {}
 
-instance = 'prod/phys03'
+instance = 'prod/global'
 
 xrd = 'root://xrootd-cms.infn.it//'
 
 for dataset in fset:
     flist = os.popen(("dasgoclient -query='instance={} file dataset={}'").format(instance,fset[fset.index(dataset)].rstrip())).read().split('\n')
-    if "TTToSemi" in dataset:
-        dictname = "ttbarSL"
-    if "TTTo2L2Nu" in dataset:
-        dictname = "ttbarDL"
-    elif "DY" in dataset:#TODO split in HT bins (xsecs?)
-        dictname = "DYJets"
-    elif "WJets" in dataset:#TODO split in HT bins (xsecs?)
-        dictname = "WJets"
-    elif "ST_t-channel_top" in dataset:
-        dictname = "st_tch_top"
-    elif "ST_t-channel_antitop" in dataset:
-        dictname = "st_tch_atop"
-    elif "ST_tW_top" in dataset:
-        dictname = "st_tw_top"
-    elif "ST_tW_antitop" in dataset:
-        dictname = "st_tw_atop"
-    elif "QCD" in dataset:#TODO split in HT or pT bins (xsecs?)
-        dictname = "QCD"
-    elif "Run201" in dataset: #all data samples have this snippet in common
-        dictname = "Data"
+    if "Summer19" in dataset:
+        dictname = "ttbar19"
+    elif "Summer20" in dataset:
+        dictname = "ttbar20"
     else:
         dictname = dataset.rstrip()
     if dictname not in fdict:
@@ -45,5 +29,5 @@ for dataset in fset:
         fdict[dictname].extend([xrd+f for f in flist if len(f) > 1])
 
 #pprint.pprint(fdict, depth=1)
-with open('datasets.json', 'w') as fp:
+with open('samplesUL16.json', 'w') as fp:
     json.dump(fdict, fp, indent=4)
