e0c7711670561613da75e6a0bba565b2b442def7
Felix Philipp Zinn <felix.zinn@rwth-aachen.de>
2025-01-17 15:30:55 +0100

    update btag_sf shell script

--- scripts/submit/btag_sf.sh
+++ scripts/submit/btag_sf.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-DATE=$(date +%y_%m_%d)
-echo "running on $DATE"
 
 # Check if the arguments file exists
 if [ ! -f "./scripts/submit/arguments.txt" ]; then
@@ -28,41 +26,67 @@ arg_lumi="${arg_array[3]}"
 scaleout="${arg_array[4]}"
 overwrite="${arg_array[5]}"
 
-chunksize=75000
+chunksize=200000
+limit=10
+max=10
 
 # check files
+basepath="/net/data_cms3a-1/fzinn/BTV/btag_sf/nobackup"
+
 datafile="hists_btag_ttbar_sf_data_"$arg_campaign"_"$arg_year"_em_BTV_Run3_"$arg_year"_Comm_MINIAODv4_NanoV12/hists_btag_ttbar_sf_data_"$arg_campaign"_"$arg_year"_em_BTV_Run3_"$arg_year"_Comm_MINIAODv4_NanoV12.coffea"
 mcfile="hists_btag_ttbar_sf_MC_"$arg_campaign"_"$arg_year"_BTV_Run3_"$arg_year"_Comm_MINIAODv4_NanoV12/hists_btag_ttbar_sf_MC_"$arg_campaign"_"$arg_year"_BTV_Run3_"$arg_year"_Comm_MINIAODv4_NanoV12.coffea"
 
-# Main running scripts
-# echo "Processing MC..."
-# if [[ ! -f $mcfile ]]; then
-#     python runner.py --json metadata/MC_"$arg_campaign"_"$arg_year"_BTV_Run3_"$arg_year"_Comm_MINIAODv4_NanoV12.json --workflow btag_ttbar_sf --campaign $arg_campaign --year $arg_year --executor $arg_executor --retries 1 -s $scaleout --chunk $chunksize --limit 10  # --max 1
-# elif [[ "$overwrite" == "true" ]]; then
-#     python runner.py --json metadata/MC_"$arg_campaign"_"$arg_year"_BTV_Run3_"$arg_year"_Comm_MINIAODv4_NanoV12.json --workflow btag_ttbar_sf --campaign $arg_campaign --year $arg_year --executor $arg_executor --retries 1 -s $scaleout --chunk $chunksize --limit 10 --overwrite  # --max 1
-# fi
+mcfile_storage="$basepath/$arg_campaign/hists_MC_${arg_campaign}.coffea"
+datafile_storage="$basepath/$arg_campaign/hists_data_${arg_campaign}.coffea"
 
-echo "Processing data..."
-if [[ ! -f $datafile ]]; then
-    python runner.py --json metadata/data_"$arg_campaign"_"$arg_year"_em_BTV_Run3_"$arg_year"_Comm_MINIAODv4_NanoV12.json --workflow btag_ttbar_sf --campaign $arg_campaign --year $arg_year --executor $arg_executor --retries 1 -s $scaleout --chunk $chunksize --limit 10  # --max 1 
+# Main running scripts
+echo "
+Processing MC..."
+mcjson="metadata/$arg_campaign/MC_"$arg_campaign"_"$arg_year"_BTV_Run3_"$arg_year"_Comm_MINIAODv4_NanoV12.json"
+# mcjson="test_my_samples.json"
+if [[ ! -f $mcfile_storage ]]; then
+    python runner.py --json $mcjson --workflow btag_ttbar_sf --campaign $arg_campaign --year $arg_year --executor $arg_executor --retries 1 -s $scaleout --chunk $chunksize --limit $limit --max $max --overwrite
+    mv $mcfile $mcfile_storage
 elif [[ "$overwrite" == "true" ]]; then
-    python runner.py --json metadata/data_"$arg_campaign"_"$arg_year"_em_BTV_Run3_"$arg_year"_Comm_MINIAODv4_NanoV12.json --workflow btag_ttbar_sf --campaign $arg_campaign --year $arg_year --executor $arg_executor --retries 1 -s $scaleout --chunk $chunksize --limit 20 --overwrite  # --max 1
+    python runner.py --json $mcjson --workflow btag_ttbar_sf --campaign $arg_campaign --year $arg_year --executor $arg_executor --retries 1 -s $scaleout --chunk $chunksize --limit $limit --max $max --overwrite
+    mv $mcfile $mcfile_storage
+else
+    echo "MC file already exists. Skipping..."
+fi
+
+# Check if the python command ran successfully
+if [[ $? -ne 0 ]]; then
+    echo "Error: MC Processing failed."
+    return 1
+fi
+
+echo "
+Processing data..."
+if [[ ! -f $datafile_storage ]]; then
+    python runner.py --json metadata/$arg_campaign/data_"$arg_campaign"_"$arg_year"_em_BTV_Run3_"$arg_year"_Comm_MINIAODv4_NanoV12.json --workflow btag_ttbar_sf --campaign $arg_campaign --year $arg_year --executor $arg_executor --retries 1 -s $scaleout --chunk $chunksize --limit $limit --max $max --overwrite
+    mv $datafile $datafile_storage
+elif [[ "$overwrite" == "true" ]]; then
+    python runner.py --json metadata/$arg_campaign/data_"$arg_campaign"_"$arg_year"_em_BTV_Run3_"$arg_year"_Comm_MINIAODv4_NanoV12.json --workflow btag_ttbar_sf --campaign $arg_campaign --year $arg_year --executor $arg_executor --retries 1 -s $scaleout --chunk $chunksize --limit $limit --max $max --overwrite
+    mv $datafile $datafile_storage
+else
+    echo "Data file already exists. Skipping..."
+fi
+
+if [[ $? -ne 0 ]]; then
+    echo "Error: Data Processing failed."
+    return 1
 fi
 
 # plots
 echo "Plotting..."
-python scripts/plotdataMC.py -i $mcfile,$datafile --lumi $arg_lumi -p btag_ttbar_sf --ext $arg_campaign -v all --log
+python scripts/plotdataMC.py -i $mcfile_storage,$datafile_storage --lumi $arg_lumi -p btag_ttbar_sf --ext $arg_campaign -v *pt,*eta,*phi --log
 
-
-# move files
-echo "Moving files..."
-basepath="/net/data_cms3a-1/fzinn/BTV/btag_sf/nobackup"
-if [[ -f $mcfile ]]; then
-    mv $mcfile $basepath/$arg_campaign/hists_MC_${arg_year}_BTV_em.coffea
-fi
-if [[ -f $datafile ]]; then
-    mv $datafile $basepath/$arg_campaign/hists_data_${arg_year}_em.coffea
+# Check if the python command ran successfully
+if [[ $? -ne 0 ]]; then
+    echo "Error: Plotting script failed."
+    return 1
 fi
 plotdir="$basepath/$arg_campaign/plots/"
 mkdir --parents $plotdir
-mv plot/BTV/btag_ttbar_sf_${arg_campaign}_${DATE}/* $basepath/$arg_campaign/plots/
+mv plot/btag_ttbar_sf_${arg_campaign}/* $plotdir
+echo "Plots saved in $plotdir"
