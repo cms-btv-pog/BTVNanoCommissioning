db0f1a05d9a7655413114a460e6e773ab24ba3a6
Livio Redard-Jacot <63054458+lredar@users.noreply.github.com>
2025-02-11 10:19:32 +0100

    Fix lumi calculation (keep pairing of runs and lumisections while sorting) (#119)

--- scripts/dump_processed.py
+++ scripts/dump_processed.py
@@ -12,10 +12,16 @@ def dump_lumi(output, fname):
             lumi.extend(output[m][f]["lumi"].value)
             run.extend(output[m][f]["run"].value)
 
-    lumi, run = np.array(sorted(lumi)), np.array(sorted(run))
+    # Sort runs and keep lumisections matched
+    run, lumi = np.array(run), np.array(lumi)
+    sorted_indices = np.lexsort((lumi, run))  # Sort by run first, then lumi
+    run = run[sorted_indices]
+    lumi = lumi[sorted_indices]
+    # Create dictionary with ls values for each run
     dicts = {}
-    for r in list(set(run)):
-        dicts[str(r)] = lumi[r == run]
+    for r in np.unique(run):
+        dicts[str(r)] = lumi[run == r]
+    # Convert to format for brilcalc
     for r in dicts.keys():
         ar = ak.singletons(ak.Array(dicts[r]))
         ars = ak.concatenate([ar, ar], axis=-1)
