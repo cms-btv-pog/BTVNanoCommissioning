d0f3a692a513e0af0d94dc3683a3842c46727668
Felix Philipp Zinn <felix.zinn@rwth-aachen.de>
2025-08-13 13:58:46 +0200

    implement full and reduced jes uncertainties

--- src/BTVNanoCommissioning/utils/AK4_parameters.py
+++ src/BTVNanoCommissioning/utils/AK4_parameters.py
@@ -181,3 +181,40 @@ correction_config = {
     },
     "prompt_dataMC": {"lumiMask": "$PROMPT_DATAMC"},
 }
+
+
+# jes sources for the full set according to
+# https://cms-jerc.web.cern.ch/Recommendations/#case-i
+JES_SOURCES = {
+    "full": {
+        "AbsoluteMPFBias",
+        "AbsoluteScale",
+        "AbsoluteStat",
+        "FlavorQCD",
+        "Fragmentation",
+        "PileUpDataMC",
+        "PileUpPtBB",
+        "PileUpPtEC1",
+        "PileUpPtEC2",
+        "PileUpPtHF",
+        "PileUpPtRef",
+        "RelativeFSR",
+        "RelativeJEREC1",
+        "RelativeJEREC2",
+        "RelativeJERHF",
+        "RelativePtBB",
+        "RelativePtEC1",
+        "RelativePtEC2",
+        "RelativePtHF",
+        "RelativeBal",
+        "RelativeSample",
+        "RelativeStatEC",
+        "RelativeStatFSR",
+        "RelativeStatHF",
+        "SinglePionECAL",
+        "SinglePionHCAL",
+        "TimePtEta",
+    },
+    "reduced": {"Total"},
+    "total": {"Total"},
+}
