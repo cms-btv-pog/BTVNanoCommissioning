dc752098a100991d4a3fc8e9856a242d28f6f069
Nico Toikka <nico.toikka@helsinki.fi>
2025-09-24 22:57:56 +0200

    fix: correct key in QCD wf

--- src/BTVNanoCommissioning/utils/histogramming/histograms/qcd_smu.py
+++ src/BTVNanoCommissioning/utils/histogramming/histograms/qcd_smu.py
@@ -6,7 +6,7 @@ def get_histograms(axes, **kwargs):
 
     hists["nmujet"] = Hist.Hist(axes["syst"], axes["n"], Hist.storage.Weight())
     hists["nssmu"] = Hist.Hist(axes["syst"], axes["n"], Hist.storage.Weight())
-    hists["dr_lmusmu"] = Hist.Hist(
+    hists["dr_lmujetsmu"] = Hist.Hist(
         axes["syst"], axes["flav"], axes["dr_s"], Hist.storage.Weight()
     )
     for i in ["soft_l"]:
